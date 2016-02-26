c1e692a8577e073b91723b913439d3ff0cc223f9
Timo Mihaljov <timo.mihaljov@solita.fi>
2016-02-26 20:03:27 +0200

    Move test server management to setup/teardown

--- it.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -eu
-trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
-mvn -B compile
-mvn -B exec:java -Dexec.mainClass=fi.solita.cd.Hello &
-mvn -B failsafe:integration-test
--- src/test/java/fi/solita/cd/HelloIT.java
+++ src/test/java/fi/solita/cd/HelloIT.java
@@ -6,13 +6,34 @@ import java.io.IOException;
 
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Request;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import spark.Spark;
 
 public class HelloIT {
 
+    public static final int SERVER_PORT = 4567;
+    public static final String SERVER_URL = "http://localhost:" + SERVER_PORT;
+
+    @BeforeClass
+    public static void startServer() {
+        Spark.port(SERVER_PORT);
+        Hello.main(new String[]{});
+        Spark.awaitInitialization();
+    }
+
+    @AfterClass
+    public static void stopServer() {
+        Spark.stop();
+    }
+
     @Test
     public void foo() throws ClientProtocolException, IOException {
-        String response = Request.Get("http://localhost:4567").execute().returnContent().asString();
+        String response = Request.Get(SERVER_URL).execute().returnContent().asString();
         assertTrue(response.contains("Hello, World!"));
     }
 
